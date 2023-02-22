e0aa386133a8c7270ef4361c633dd9f495462f09
BenjaminHecker <benhecker7@gmail.com>
2023-02-22 12:01:25 -0500

    Basic throwing knife mechanics

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [SerializeField] private GameObject knifePrefab;
-    [SerializeField] private float throwSpeed = 10f;
-    [SerializeField] private float rotateSpeed = 1f;
+    [SerializeField] private int knifeCapacity = 3;
+    private int knifeCount;
+
+    [SerializeField] private ThrowingKnife knifePrefab;
+    [SerializeField] private float travelSpeed = 10f;
+    [SerializeField] private float turnSpeed = 1f;
+
+    private void Awake()
+    {
+        knifeCount = knifeCapacity;
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // throw knife
+            ThrowKnife();
         }
+    }
+
+    private void ThrowKnife()
+    {
+        if (knifeCount > 0)
+        {
+            Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+            if (dir == Vector3.zero)
+                dir = Vector3.right;
+
+            ThrowingKnife knife = Instantiate(knifePrefab);
+            knife.Setup(this, transform, travelSpeed, turnSpeed, dir.normalized);
+
+
+            knifeCount--;
+        }
+    }
+
+    public void RegainKnife()
+    {
+        knifeCount++;
     }
 }
--- Assets/Scripts/ThrowingKnife.cs
+++ Assets/Scripts/ThrowingKnife.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class ThrowingKnife : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     private Transform target;
     private float travelSpeed = 10f;
     private float turnSpeed = 1f;
 
-    public void Setup(Transform target, float travelSpeed, float turnSpeed)
+    private PlayerAttack playerAttack;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Setup(PlayerAttack playerAttack, Transform target, float travelSpeed, float turnSpeed, Vector3 dir)
+    {
+        this.playerAttack = playerAttack;
         this.target = target;
         this.travelSpeed = travelSpeed;
         this.turnSpeed = turnSpeed;
+
+        transform.position = target.position + dir;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
+        rb.velocity = dir * travelSpeed;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 dir = (Vector2) target.position - rb.position;
+        float rotateAmount = Vector3.Cross(dir.normalized, transform.up).z;
+        rb.angularVelocity = -turnSpeed * rotateAmount;
+        rb.velocity = transform.up * travelSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerAttack.RegainKnife();
+            Destroy(gameObject);
+        }
     }
 }
